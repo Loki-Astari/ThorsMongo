ef2e8f75ba2fb20bd08fe17742776c7b24349499
Loki Astari <Loki.Astari@gmail.com>
2024-08-08 01:51:55 -0700

    Allow multiple levels in the filter properties

--- src/ThorsMongo/test/MongoQueryTest.cpp
+++ src/ThorsMongo/test/MongoQueryTest.cpp
@@ -8,6 +8,8 @@
 #include <sstream>
 
 ThorsMongo_CreateFilter(FindByPersonNameEq, Eq, People, name);
+ThorsMongo_CreateFilter(FindByPersonAddressCCEq, Eq, People, address, city);
+ThorsMongo_CreateFilter(FindByFamilyMotherAddress, Eq, Family, mother, address, code);
 
 TEST(MongoQueryTest, CreateFilterOneLevel)
 {
@@ -17,5 +19,25 @@ TEST(MongoQueryTest, CreateFilterOneLevel)
     s << ThorsAnvil::Serialize::jsonExporter(FindByPersonNameEq{"Tim"}, PrinterInterface::PrinterConfig{PrinterInterface::OutputType::Stream});
 
     EXPECT_EQ(R"({"name":{"$eq":"Tim"}})", s.str());
-
 }
+
+TEST(MongoQueryTest, CreateFilterTwoLevel)
+{
+    std::stringstream   s;
+
+    using ThorsAnvil::Serialize::PrinterInterface;
+    s << ThorsAnvil::Serialize::jsonExporter(FindByPersonAddressCCEq{"London"}, PrinterInterface::PrinterConfig{PrinterInterface::OutputType::Stream});
+
+    EXPECT_EQ(R"({"address.city":{"$eq":"London"}})", s.str());
+}
+
+TEST(MongoQueryTest, CreateFilterThreeLevel)
+{
+    std::stringstream   s;
+
+    using ThorsAnvil::Serialize::PrinterInterface;
+    s << ThorsAnvil::Serialize::jsonExporter(FindByFamilyMotherAddress{12}, PrinterInterface::PrinterConfig{PrinterInterface::OutputType::Stream});
+
+    EXPECT_EQ(R"({"mother.address.code":{"$eq":12}})", s.str());
+}
+
